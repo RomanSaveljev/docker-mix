d4731788ffbabb8cead525fd8c2ebc8cc5219a7f
Roman Saveljev <roman.saveljev@haltian.com>
2015-09-07 01:37:31 +0300

    Refactor MAINTAINER for applyTo() support

--- src/commands/maintainer.coffee
+++ src/commands/maintainer.coffee
@@ -1,10 +1,8 @@
 class Maintainer
   constructor: (@maintainer) ->
     throw new Error('Must pass maintainer name') unless @maintainer
-  toString: -> "#{@keyword()} #{@maintainer}"
-  keyword: -> "MAINTAINER"
-  toString: -> "#{@keyword()} #{@maintainer}"
-  combines: (command) -> false
+  applyTo: (context, dockerfile) ->
+    dockerfile.push("MAINTAINER #{@maintainer}")
   overrides: (command) -> true
 
 module.exports = Maintainer
--- test/commands/maintainer.js
+++ test/commands/maintainer.js
@@ -2,24 +2,14 @@ var Maintainer = require('../../lib/commands/maintainer');
 var should = require('should');
 
 describe('Maintainer', function() {
-  it('has MAINTAINER keyword', function() {
-    var maintainer = new Maintainer('me');
-    should(maintainer.keyword()).be.equal('MAINTAINER');
-  });
+  var dockerfile = [];
   it('constructor throws without arguments', function() {
     should(function() {new Maintainer()}).throw();
   });
-  it('assigns maintainer', function() {
-    var maintainer = new Maintainer('John Doe');
-    should(maintainer.maintainer).be.equal('John Doe');
-  });
-  it('uses maintainer in toString()', function() {
+  it('uses maintainer in applyTo()', function() {
     var maintainer = new Maintainer('Abby Foo');
-    should(maintainer.toString()).be.equal('MAINTAINER Abby Foo');
-  });
-  it('does not combine', function() {
-    var maintainer = new Maintainer('someone');
-    should(maintainer.combines()).be.false();
+    maintainer.applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('MAINTAINER Abby Foo');
   });
   it('overrides', function() {
     var maintainer = new Maintainer('someone');
