68095a05d402d2459d8f13d0a4d08d32b653de24
Roman Saveljev <roman.saveljev@haltian.com>
2015-09-07 21:59:41 +0300

    Implement dockerfile-functions

---
+++ src/dockerfile-functions.coffee
@@ -0,0 +1,51 @@
+Env = require('./commands/env')
+MultiEnv = require('./commands/multi-env')
+Run = require('./commands/run')
+MultiRun = require('./commands/multi-run')
+Expose = require('./commands/expose')
+MultiExpose = require('./commands/multi-expose')
+Volume = require('./commands/volume')
+MultiVolume = require('./commands/multi-volume')
+Label = require('./commands/label')
+MultiLabel = require('./commands/multi-label')
+ContextCopy = require('./commands/context-copy')
+MultiContextCopy = require('./commands/multi-context-copy')
+
+module.exports.sameType = (a, type) -> a.constructor == type
+
+module.exports.sameCommand = (a, b) -> sameType(a, b.constructor)
+
+module.exports.combinesTo = (type) ->
+  switch type
+    when Env, MultiEnv then return MultiEnv
+    when Expose, MultiExpose then return MultiExpose
+    when Label, MultiLabel then return MultiLabel
+    when Run, MultiRun then return MultiRun
+    when Volume, MultiVolume then return MultiVolume
+    when ContextCopy, MultiContextCopy then return MultiContextCopy
+    else return undefined
+
+module.exports.aggregateRegion = (list, index) ->
+  next = index + 1
+  # Index should point at the beginning of a suitable region
+  type = list[index].constructor
+  ctor = module.exports.combinesTo(type)
+  return next unless ctor?
+  # Every combinable command becomes a multi-command (with a potential to collect
+  # more than one command)
+  aggregated = new ctor(list[index])
+  aggregated.next = list[index].next
+  list[index] = aggregated
+  return next unless list[index].next.length == 0
+  while list.length > next
+    return next if module.exports.combinesTo(list[next].constructor) != ctor or list[next].next.length != 0
+    list[index] = new ctor(list[index], list[next])
+    list[(next)..(next)] = []
+  return next
+
+# Updates the list in-place, handles continuous regions
+module.exports.aggregate = (list) ->
+  index = 0
+  while (index < list.length)
+    index = module.exports.aggregateRegion(list, index)
+  return list
---
+++ test/dockerfile-functions.js
@@ -0,0 +1,110 @@
+var functions = require('../lib/dockerfile-functions');
+var should = require('should');
+var Run = require('../lib/commands/run');
+var MultiRun = require('../lib/commands/multi-run');
+var Expose = require('../lib/commands/expose');
+var MultiExpose = require('../lib/commands/multi-expose');
+
+function prepareCommand(cmd) {
+  cmd.next = [];
+  return cmd;
+}
+
+describe('AggregateRegion', function() {
+  var dockerfile = [];
+  it('returns next index to check, if first element type can not be combined', function() {
+    var list = [prepareCommand({}), prepareCommand(new Run('echo 123'))];
+    var index = functions.aggregateRegion(list, 0);
+    should(index).be.equal(1);
+    should(list.length).be.equal(2);
+  });
+  it('single combinable commands with children become multi-commands', function() {
+    var command = prepareCommand(new Run('echo 123'));
+    command.next.push({a: 3});
+    var list = [command];
+    var index = functions.aggregateRegion(list, 0);
+    should(index).be.equal(1);
+    should(list.length).be.equal(1);
+    should(list[0]).be.instanceof(MultiRun);
+    should(list[0].next).be.Array();
+    should(list[0].next).be.equal(command.next);
+  });
+  it('single combinable commands without children become multi-commands', function() {
+    var command = prepareCommand(new Run('echo 123'));
+    var list = [command];
+    var index = functions.aggregateRegion(list, 0);
+    should(index).be.equal(1);
+    should(list.length).be.equal(1);
+    should(list[0]).be.instanceof(MultiRun);
+    should(list[0].next).be.Array();
+    should(list[0].next).be.equal(command.next);
+    list[0].applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('RUN echo 123');
+  });
+  it('collects together combinable child-free commands', function() {
+    var command = prepareCommand(new Run('echo 123'));
+    var command2 = prepareCommand(new Run('echo 456'));
+    var list = [command, command2];
+    var index = functions.aggregateRegion(list, 0);
+    should(index).be.equal(1);
+    should(list.length).be.equal(1);
+    should(list[0]).be.instanceof(MultiRun);
+    var dockerfile = [];
+    list[0].applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('RUN echo 123 && echo 456');
+  });
+  it('stops collecting at first command with children', function() {
+    var command = prepareCommand(new Run('echo 123'));
+    var command2 = prepareCommand(new Run('echo 456'));
+    command2.next.push({});
+    var list = [command, command2];
+    var index = functions.aggregateRegion(list, 0);
+    should(index).be.equal(1);
+    should(list.length).be.equal(2);
+    should(list[0]).be.instanceof(MultiRun);
+    list[0].applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('RUN echo 123');
+    should(list[1]).be.equal(command2);
+  });
+  it('collects until the first different command', function() {
+    var command = prepareCommand(new Run('echo 123'));
+    var command2 = prepareCommand(new Expose(12));
+    var list = [command, command2];
+    var index = functions.aggregateRegion(list, 0);
+    should(index).be.equal(1);
+    should(list.length).be.equal(2);
+    should(list[0]).be.instanceof(MultiRun);
+    list[0].applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('RUN echo 123');
+    should(list[1]).be.equal(command2);
+  });
+  it('combines multi-command with a single command', function() {
+    var command = prepareCommand(new MultiRun(new Run('echo 123')));
+    var command2 = prepareCommand(new Run('echo 456'));
+    var list = [command, command2];
+    var index = functions.aggregateRegion(list, 0);
+    should(index).be.equal(1);
+    should(list.length).be.equal(1);
+    should(list[0]).be.instanceof(MultiRun);
+    list[0].applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('RUN echo 123 && echo 456');
+  });
+});
+
+describe('Aggregate', function() {
+  it('aggregates all regions', function() {
+    var command = prepareCommand(new Run('echo 123'));
+    var command2 = prepareCommand(new Run('echo 456'));
+    var command3 = prepareCommand(new Expose(56));
+    var command4 = prepareCommand(new Expose(57));
+    var list = [command, command2, command3, command4];
+    functions.aggregate(list, 0);
+    should(list.length).be.equal(2);
+    should(list[0]).be.instanceof(MultiRun);
+    list[0].applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('RUN echo 123 && echo 456');
+    should(list[1]).be.instanceof(MultiExpose);
+    list[1].applyTo({}, dockerfile);
+    should(dockerfile.pop()).be.equal('EXPOSE 56 57');
+  });
+});
